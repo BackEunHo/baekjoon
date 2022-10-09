f22fd64e9468d05b062d6ab1b1fa8b0de52e7990
BackEunHo <50124989+BackEunHo@users.noreply.github.com>
2022-10-09 16:23:30 +0900

    [Silver II] Title: 좌표 압축, Time: 2016 ms, Memory: 148188 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.py
@@ -0,0 +1,7 @@
+import sys
+n = int(sys.stdin.readline())
+arr = list(map(int,sys.stdin.readline().split())) # 2 4 -10 4 -9
+arr2 = list(sorted(set(arr)))#[-10 -9 2 4]
+dic = {arr2[i]:i for i in range (len(arr2))} 
+for i in arr:
+  print(dic[i],end=' ')
