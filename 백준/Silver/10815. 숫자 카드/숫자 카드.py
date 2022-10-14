311a954c838c575958f7e2667bea1932bc4c460e
BackEunHo <50124989+BackEunHo@users.noreply.github.com>
2022-10-14 14:27:21 +0900

    [Silver V] Title: 숫자 카드, Time: 712 ms, Memory: 218192 KB -BaekjoonHub

--- 백준/Silver/10815. 숫자 카드/숫자 카드.py
+++ 백준/Silver/10815. 숫자 카드/숫자 카드.py
@@ -1,26 +1,26 @@
-import sys
+import sys 
 
-n = int(input())
-card = list(map(int, sys.stdin.readline().split()))
-m = int(input())
-check = list(map(int, sys.stdin.readline().split()))
+def binary_search(arr,key): # arr의 key값의 인덱스를 반환 
+    low = 0
+    high = len(arr)-1
+    while high >= low:
+        mid = (low+high) // 2
+        if key < arr[mid]:
+            high = mid-1
+        elif key > arr[mid]:
+            low = mid + 1
+        else: # key == arr[mid]
+            return mid 
+    return 'no key' # key not in arr일때
 
-card.sort()
+sang_num = int(sys.stdin.readline())
+sang_card = list(map(int,sys.stdin.readline().split()))
+find_num = int(sys.stdin.readline())
+find_card = list(map(int,sys.stdin.readline().split()))
 
-def binary_search(array, target, start, end):
-    while start <= end:
-        mid = (start + end) // 2
-        if array[mid] == target:
-            return mid
-        elif array[mid] > target:
-            end = mid - 1
-        else:
-            start = mid + 1
-    return None
-
-
-for i in range(m):
-    if binary_search(card, check[i], 0, n - 1) is not None:
+sang_card.sort()
+for i in find_card: # [10 9 -5 2 3 4 5 -10]
+    if binary_search(sang_card,i) != 'no key':
         print(1, end=' ')
     else:
         print(0, end=' ')
